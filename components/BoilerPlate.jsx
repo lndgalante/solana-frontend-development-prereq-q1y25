34732e4b20639ec761dade94bbb5fc99a8a7cd5e
Leonardo Galante <lndgalante@gmail.com>
2025-01-19 23:41:55 -0300

    feat: Format code. Use react 18.2.0

--- components/BoilerPlate.jsx
+++ components/BoilerPlate.jsx
@@ -1,18 +1,19 @@
 const BoilerPlate = () => {
-    return (
-        <main className="bg-[#161b19] min-h-screen text-white">
-            <div className="w-full h-screen flex justify-center items-center">
-                <div className="flex flex-col text-center items-center border-2 p-10 rounded-lg bg-zinc-800 border-zinc-600">
-                    <h1 className="text-4xl italic">
-                        your turn to build!
-                    </h1>
-                    <a href='/' className="rounded-lg mt-8 py-2 w-1/2 bg-turbine-green font-medium transition-all duration-200 border-2 border-transparent hover:border-turbine-green hover:bg-transparent">
-                        return home
-                    </a>
-                </div>
-            </div>
-        </main>
-    )
+	return (
+		<main className="bg-[#161b19] min-h-screen text-white">
+			<div className="flex items-center justify-center w-full h-screen">
+				<div className="flex flex-col items-center p-10 text-center border-2 rounded-lg bg-zinc-800 border-zinc-600">
+					<h1 className="text-4xl italic">your turn to build!</h1>
+					<a
+						href="/"
+						className="w-1/2 py-2 mt-8 font-medium transition-all duration-200 border-2 border-transparent rounded-lg bg-turbine-green hover:border-turbine-green hover:bg-transparent"
+					>
+						return home
+					</a>
+				</div>
+			</div>
+		</main>
+	);
 };
 
 export default BoilerPlate;
--- contexts/WalletContextProvider.jsx
+++ contexts/WalletContextProvider.jsx
@@ -1,25 +1,23 @@
-import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
-import { WalletModalProvider } from "@solana/wallet-adapter-react-ui"
-import * as walletAdapterWallets from '@solana/wallet-adapter-wallets'
-import * as web3 from '@solana/web3.js';
-require('@solana/wallet-adapter-react-ui/styles.css');
+import {
+	ConnectionProvider,
+	WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import * as walletAdapterWallets from "@solana/wallet-adapter-wallets";
+import * as web3 from "@solana/web3.js";
+require("@solana/wallet-adapter-react-ui/styles.css");
 
 const WalletContextProvider = ({ children }) => {
+	const endpoint = web3.clusterApiUrl("devnet");
+	const wallets = [new walletAdapterWallets.PhantomWalletAdapter()];
 
-    const endpoint = web3.clusterApiUrl('devnet');
-    const wallets = [
-        new walletAdapterWallets.PhantomWalletAdapter()
-    ];
-
-    return (
-        <ConnectionProvider endpoint={endpoint}>
-            <WalletProvider wallets={wallets}>
-                <WalletModalProvider>
-                    {children}
-                </WalletModalProvider>
-            </WalletProvider>
-        </ConnectionProvider>
-    );
+	return (
+		<ConnectionProvider endpoint={endpoint}>
+			<WalletProvider wallets={wallets}>
+				<WalletModalProvider>{children}</WalletModalProvider>
+			</WalletProvider>
+		</ConnectionProvider>
+	);
 };
 
 export default WalletContextProvider;
